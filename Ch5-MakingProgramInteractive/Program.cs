8b8834a22f995cfc04d00a94c08992a77fc9cf2e
Rashmi S <s.rashmi94@gmail.com>
2021-03-30 13:23:59 -0400

    Created example for Ch5

---
+++ Ch5-MakingProgramInteractive/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch5_MakingProgramInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------Example 1 (WriteLine)-------------");
+            Console.WriteLine("Hello");
+            Console.WriteLine("How are you");
+            //Console.ReadLine();
+
+            Console.WriteLine("---------------Example 2(Write)-------------");
+            Console.Write("Hello");
+            Console.Write("How are you");
+            //Console.Read();
+
+            Console.WriteLine("\n---------------Example 3 (Alternate method of including using static system.console)-------------");
+            WriteLine("Hello");
+            Write("How are you");
+            Console.ReadLine();
+        }
+    }
+}
